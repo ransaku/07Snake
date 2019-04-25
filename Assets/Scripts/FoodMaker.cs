62b897603b58b68ed05e07c9a6723eef55e482e8
ransaku <huyrt1994@gmail.com>
2019-04-25 13:22:08 +0800

    Revert "Revert "Finished the record""
    This reverts commit 64aab471e64f61aac829e2be91cfee0b0fe56de3.

---
+++ Assets/Scripts/FoodMaker.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class FoodMaker : MonoBehaviour
+{
+    //单例模式
+    private static FoodMaker _instance;
+    public static FoodMaker Instance
+    {
+        get { return _instance; }
+    }
+    
+    
+    //设置生成食物的上下最大范围
+    public int xBorder = 20;
+    public int yBorder = 12;
+    //利用offset来处理初始位置的左右坐标偏差
+    public int xOffset = 7;
+    public int yOffset = 4;
+
+    //获取需要生成的食物的prefab
+    public GameObject foodPrefab;
+    //定义一个存放食物图片的数组
+    public Sprite[] foodSprites;
+    private Transform foodHolder;
+    
+    public GameObject rewardPrefab;//获取奖励物品的prefab
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    void Start()
+    {
+        foodHolder = GameObject.Find("FoodHolder").transform;
+        FoodPop(false);
+    }
+
+    public void FoodPop(bool isReward)
+    {
+        //生成一个食物贴图的索引值
+        int index = Random.Range(0, foodSprites.Length);
+        //实例化一个预制体
+        GameObject food = Instantiate(foodPrefab);
+        //给食物的image组件贴图赋值
+        food.GetComponent<Image>().sprite = foodSprites[index];
+        //将食物生成在FoodHolder组件上
+        food.transform.SetParent(foodHolder,false);//false 不自动转换坐标
+        
+        //计算并设置生成食物的坐标
+        int x = Random.Range(-xBorder + xOffset, xBorder);
+        int y = Random.Range(-yBorder + yOffset, yBorder);
+        food.transform.localPosition = new Vector3(x*30,y*30,0);
+
+        if (isReward)
+        {
+            //实例化一个预制体
+            GameObject reward = Instantiate(rewardPrefab);
+            //将奖励生成在FoodHolder组件上
+            reward.transform.SetParent(foodHolder,false);//false 不自动转换坐标
+            //计算并设置生成奖励的坐标
+            x = Random.Range(-xBorder + xOffset, xBorder);
+            y = Random.Range(-yBorder + yOffset, yBorder);
+            reward.transform.localPosition = new Vector3(x*30,y*30,0);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+	//需要在其他脚本调用该脚本函数使用单例模式
+	private static GameController _instance;
+	public static GameController Instance
+	{
+		get { return _instance; }
+	}
+
+	private void Awake()
+	{
+		_instance = this;
+	}
+	
+	public int score=0;
+	public int length=0;
+	public Text msgText;
+	public Text scoreText;
+	public Text lengthText;
+	public Image bgImage;
+	private Color bgColor;
+
+	private void Update()
+	{
+		switch (score/100)
+		{
+			case 0:
+			case 1:
+				break;
+			case 2:
+			case 3:
+				ColorUtility.TryParseHtmlString("#CCEEFFFF", out bgColor);
+				bgImage.color = bgColor;
+				msgText.text = "阶段" + 2;
+				break;
+			case 5:
+			case 6:
+				ColorUtility.TryParseHtmlString("#ccffdbff", out bgColor);
+				bgImage.color = bgColor;
+				msgText.text = "阶段" + 3;
+				break;
+			case 7:
+			case 8:
+				ColorUtility.TryParseHtmlString("#ebffccff", out bgColor);
+				bgImage.color = bgColor;
+				msgText.text = "阶段" + 4;
+				break;
+			case 9:
+			case 10:
+				ColorUtility.TryParseHtmlString("#fff3ccff", out bgColor);
+				bgImage.color = bgColor;
+				msgText.text = "阶段" + 5;
+				break;
+			default:
+				ColorUtility.TryParseHtmlString("#ffdaccff", out bgColor);
+				bgImage.color = bgColor;
+				msgText.text = "无尽阶段";
+				break;
+			
+		}
+	}
+
+	public void UpdateUI(int s = 5, int l= 1)
+	{
+		score += s;
+		length += l;
+		scoreText.text = "得分：\n" + score;
+		lengthText.text = "长度：\n" + length;
+	}
+}
